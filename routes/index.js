677642e4f0efce09d438c860e9ce576961311beb
carlngan <carl@salonfrtondesk.com>
2016-03-01 19:27:49 -0800

    added signup process

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
+var environment = process.env.NODE_ENV;
 var express = require('express');
 var router = express.Router();
+
+var request = require('request');
+
+var config = require('../config/config.json')[environment];
+var API_URL = config.apiUrl;
 
 /*
 *   VIEWS
@@ -7,6 +13,42 @@ var router = express.Router();
 // GET - signup
 router.get('/signup', function (req, res) {
     res.render("signup");
+});
+
+// POST - signup
+router.post('/signup', function (req, res) {
+    request({
+            method: "POST",
+            uri: API_URL+"/providers",
+            json: {
+                provider:{
+                    name: req.body.company,
+                    domain: req.body.domain
+                },
+                employee:{
+                    name:{
+                        first: req.body.fname,
+                        last: req.body.lname
+                    },
+                    title: "owner",
+                    email: req.body.email,
+                    password:req.body.password
+                }
+            }
+        },
+        function (error, response, body) {
+            if (error) {
+                return res.status(500).send({_code:"SITE002", _msg:"Connection to API failed. Please try again later."});
+            }
+            else if(response.statusCode == 500){
+                res.status(response.statusCode).send(body);
+            }
+            else{
+                body.success = "success";
+                res.status(200).send(body);
+            }
+        });
+
 });
 
 // GET - Index
